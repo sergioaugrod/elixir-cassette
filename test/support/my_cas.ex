62af19cc5c922343cd2d374df3b3af09cd588c6b
Ricardo Hermida Ruiz <rhruiz@gmail.com>
2017-10-13 18:01:58 -0300

    Run credo and dogma on travis

--- test/support/my_cas.ex
+++ test/support/my_cas.ex
@@ -2,6 +2,6 @@ defmodule MyCas do
   @moduledoc false
 
   use Cassette.Support, process_name: :MyCasServer, config:
-    %{ Cassette.Config.default | base_authority: "ME",
-      password: {:system, "CASSETTE_TEST_PASSWORD"} }
+    %{Cassette.Config.default | base_authority: "ME",
+      password: {:system, "CASSETTE_TEST_PASSWORD"}}
 end
